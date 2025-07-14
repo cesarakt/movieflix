33bb001e7d52e584cbc4f29ba2cae44ac3ba76bd
Cesar Mazzaro <cesarmazzaro@gmail.com>
2025-07-14 20:21:21 -0300

    feat: adição das rotas para salvar,buscar todas, buscar por id, deletar por id no controller e service da app

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -3,23 +3,36 @@ package br.com.movieflix.controller;
 import br.com.movieflix.entity.Category;
 import br.com.movieflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movieflix/category")
 @RequiredArgsConstructor //Utilizado para injeção de dependências
 public class CategoryController {
 
-    private final CategoryService categoryService;
+    private final CategoryService categoryService; //Injeção de dependência via @RequiredArgsConstructor
 
     @GetMapping()
     public List<Category> getAllCategories () {
         return categoryService.findAll();
     }
 
+    @PostMapping()
+    public Category saveCategory (@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @GetMapping("/{id}")
+    public Category getByCategoryId (@PathVariable Long id) {
+        Optional<Category> optionalCategory = categoryService.findById(id);
+        return optionalCategory.orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteByCategoryId (@PathVariable Long id) {
+        categoryService.deleteCategoryById(id);
+    }
 }
--- src/main/java/br/com/movieflix/service/CategoryService.java
+++ src/main/java/br/com/movieflix/service/CategoryService.java
@@ -4,8 +4,10 @@ import br.com.movieflix.entity.Category;
 import br.com.movieflix.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +17,17 @@ public class CategoryService {
 
     public List<Category> findAll() {
         return repository.findAll();
+    }
+
+    public Category saveCategory(Category category) {
+        return repository.save(category);
+    }
+
+    public Optional<Category> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deleteCategoryById (Long id) {
+        repository.deleteById(id);
     }
 }
