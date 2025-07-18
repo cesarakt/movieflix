e6b033b73f7cfeb3282c5e177d935511749e09d5
Cesar Mazzaro <cesarmazzaro@gmail.com>
2025-07-17 22:05:52 -0300

    fix: adicionando a consulta de streamings e categories para adicionar na lista de relações ao salvar um filme

--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -1,10 +1,13 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Category;
 import br.com.movieflix.entity.Movie;
+import br.com.movieflix.entity.Streaming;
 import br.com.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,14 +16,35 @@ import java.util.Optional;
 public class MovieService {
 
     private final MovieRepository repository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public List<Movie> findAll() {
         return repository.findAll();
     }
 
     public Movie save(Movie movie) {
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
         return repository.save(movie);
     }
+
+    public List<Category> findCategories (List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<Category>();
+        categories
+                .forEach(category -> categoryService.findById(category.getId())
+                        .ifPresent(c -> categoriesFound.add(c)));
+        return categoriesFound;
+    }
+
+    public List<Streaming> findStreamings (List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<Streaming>();
+        streamings
+                .forEach(streaming -> streamingService.findById(streaming.getId())
+                        .ifPresent(s -> streamingsFound.add(s)));
+        return streamingsFound;
+    }
+
 
     public Optional<Movie> findById(Long id) {
         return repository.findById(id);
